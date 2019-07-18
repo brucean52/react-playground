8d3f186af688d80e3b1d089e948b1463997c5535
Bruce An <Bruce.An@marklogic.com>
2019-07-18 12:04:41 -0700

    added reactstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Input, Button } from 'reactstrap';
 
 class Home extends Component {
   // class components has state
@@ -40,11 +41,11 @@ class Home extends Component {
         <h2>Simple Form</h2>
         {/* Inputs implemented in different ways */}
         <form >
-          <input type="text" placeholder='Enter Title' value={title} onChange={e => this.setState({title: e.target.value})}/>
-          <input type="text" placeholder='Enter Description' value={description} onChange={({target}) => this.setState({description: target.value})}/>
-          <button className='btn' onClick={this.handleSubmit} >Submit</button>
+          <Input type="text" placeholder='Enter Title' value={title} onChange={e => this.setState({title: e.target.value})}/>
+          <Input type="text" placeholder='Enter Description' value={description} onChange={({target}) => this.setState({description: target.value})}/>
+          <Button className='btn' color="primary" onClick={this.handleSubmit} >Submit</Button>
           {/* binding this is usually done in constructor */}
-          <button className='btn'  onClick={this.handleClear.bind(this)} >Clear</button>
+          <Button className='btn' color="danger" onClick={this.handleClear.bind(this)} >Clear</Button>
         </form>
         <div>{outputMessage}</div>
       </div>
