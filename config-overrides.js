a21242820813fb20cd862b6b150d682f738350b8
Bruce An <Bruce.An@marklogic.com>
2020-05-08 11:37:08 -0700

    typescript fail

--- config-overrides.js
+++ config-overrides.js
@@ -3,11 +3,18 @@ const path = require('path')
 const themeVariables = require('@marklogic/design-system/src/theme-variables.json')
 
 module.exports = override(
+
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
      style: true,
-  }),
+  }
+  // {
+  //   libraryName: '@marklogic/design-system',
+  //   libraryDirectory: 'src',
+  //   style: true
+  // }
+  ),
  addLessLoader({
    javascriptEnabled: true,
    modifyVars: themeVariables
@@ -17,7 +24,7 @@ module.exports = override(
   /@marklogic\/design-system/, // Required for @marklogic/design-system to compile
   // In a real environment (where node_modules paths are guaranteed),
   // you may use /@marklogic\/design-system/ for specificity
-]),
+])
 )
 // Refer to theme vars below
 // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { MLButton } from '@marklogic/design-system'
+import { MLButton, MLAlert, MLPopover } from '@marklogic/design-system'
 
 const Home = () => {
 
   return (
     <div>
       <MLButton>Test</MLButton>
+    <MLAlert/>
     </div>
   );
 }
